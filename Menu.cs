699a8738f8fc97135c53cfff4cac1acf3a61b111
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-03-02 16:32:33 +0100

    First commit

---
+++ Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace MiniprojectSQL
+{
+    public class Menu
+    {
+        public static void TitleScreen()
+        {
+            string title = @"
+ __      __             __   ___________.__             ___________                     __                 
+/  \    /  \___________|  | _\__    ___/|__| _____   ___\__    ___/___________    ____ |  | __ ___________ 
+\   \/\/   /  _ \_  __ \  |/ / |    |   |  |/     \_/ __ \|    |  \_  __ \__  \ _/ ___\|  |/ // __ \_  __ \
+ \        (  <_> )  | \/    <  |    |   |  |  Y Y  \  ___/|    |   |  | \// __ \\  \___|    <\  ___/|  | \/
+  \__/\  / \____/|__|  |__|_ \ |____|   |__|__|_|  /\___  >____|   |__|  (____  /\___  >__|_ \\___  >__|   
+       \/                   \/                   \/     \/                    \/     \/     \/    \/       ";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+
+        public static void MenuOptions()
+        {
+            //Change foreground color in a string using this example: "\u001b[32mOption 1."
+            //To reset color on the same string you can do like this: "\u001b[32mOption 1.\u001b[0m"
+            Console.WriteLine("\u001b[32mOption 1.\u001b[0m");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniprojectSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.TitleScreen();
+            Menu.MenuOptions();
+            Console.ReadLine();
+        }
+    }
+}
